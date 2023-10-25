1a2f0c826962514eb7166b7cc6d8ac0fe3b2ad3c
fiyaznkp <fiyaznkp@gmail.com>
2023-10-25 11:43:52 +0530

    new

---
+++ ReactBadge/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  
+const [counter,setCounter] = useState(10)
+
+const addValue =()=>{
+  if(counter<15){
+ setCounter(counter+1)
+  }
+}
+const decreaseValue=() =>{
+  if(counter>0){
+  setCounter(counter -1)
+  }
+  
+}
+  return (
+    <>
+    <h1>Welcome</h1>
+    <h2>Counter Value {counter}</h2>
+    <button onClick={addValue}>Add Value</button>
+    <button onClick={decreaseValue}>Delete Value</button>
+    </>
+  )
+   
+  
+}
+
+export default App
